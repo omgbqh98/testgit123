9b88c414a0f946321501021f062cc481594b530e
HuynhBQ <huynhbq@rikkeisoft.com>
2021-06-06 13:52:54 +0700

    su ly confic111

--- src/test.java
+++ src/test.java
@@ -1,26 +1,21 @@
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class test {
     public static void main(String[] args) {
-        List<String> collection = Arrays.asList("huynh", "98", "ok chua");
+        List<Integer> integers = new ArrayList<>();
+        integers.add(1);
+        integers.add(2);
+        integers.add(3);
+        integers.add(4);
 
-        List<Integer> integers = Arrays.asList(1,2,3,4,5,6,6,5,3,2,2);
-
-        integers.stream().sorted().limit(4).forEach(System.out::println);
-
-//        collection.stream().forEach(s -> System.out.println(s));
-
-//        List<Integer> stream = collection.stream().collect(Collectors.toList());
-
-//        System.out.println(result);
-
-        Iterator<String> iterator = collection.iterator();
-//okokokokok
+        for (Integer i : integers) {
+            if (i == 2) {
+                continue;
+            }
+            System.out.println(i);
+        }
     }
 }
 
